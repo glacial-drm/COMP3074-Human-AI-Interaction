5e78b1e10947b4b53096e6880e69f97104a26c94
Glace <danieladetokunbo05@gmail.com>
2025-10-24 15:56:04 +0100

    coursework cope

---
+++ coursework/main.py
@@ -0,0 +1,17 @@
+import nltk
+
+def process(user_input):
+    tokens = nltk.word_tokenize(user_input)
+    return tokens
+
+def main():
+    while True:
+        user_input = input("Text goes here: ")
+        if user_input.lower() == 'exit':
+            print("Goodbye!")
+            break
+        processed_input = process(user_input)
+        print(f"Bot: You said {processed_input}")
+
+if __name__ == "__main__":
+    main()
